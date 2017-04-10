7ffa49f0cad3f20aa66aeb7cd98beaf36f16e123
Jacob Reynolds <JacobReynolds@users.noreply.github.com>
2017-04-09 21:20:00 -0500

    Update resetRateLimits.js

--- blinkie/resetRateLimits.js
+++ blinkie/resetRateLimits.js
@@ -3,30 +3,25 @@ var querystring = require('querystring');
 var fs = require('fs');
 
 var resetDomain = {
-	host: process.env.SSRF_HOST,
-	port: process.env.SSRF_HOST_PORT,
-	path: "/external/resetRateLimits",
-	method: "POST",
-	headers: {
-		'Content-Type': 'application/x-www-form-urlencoded'
-	}
+    host: process.env.SSRF_HOST,
+    port: process.env.SSRF_HOST_PORT,
+    path: "/external/resetRateLimits",
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
 };
 
-exports.resetRateLimits = function () {
-	var creds;
-	fs.readFile(__dirname + '/.creds/apiKey.json', 'utf8', function (err, data) {
-		if (err) throw err;
-		creds = JSON.parse(data);
-		var postData = {
-			apiKey: creds.apiKey
-		}
-		postData = querystring.stringify(postData);
-		resetDomain.headers['Content-Length'] = Buffer.byteLength(postData);
-		var req = http.request(resetDomain, null);
-		console.log('running');
-		req.write(postData);
-		req.end();
-	});
+exports.resetRateLimits = function() {
+    var postData = {
+        apiKey: process.env.BLINKIE_KEY
+    }
+    postData = querystring.stringify(postData);
+    resetDomain.headers['Content-Length'] = Buffer.byteLength(postData);
+    var req = http.request(resetDomain, null);
+    console.log('running');
+    req.write(postData);
+    req.end();
 }
 
 exports.resetRateLimits();
